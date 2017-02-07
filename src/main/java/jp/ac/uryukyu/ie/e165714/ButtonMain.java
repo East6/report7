ba9c558fb9da0d2dfe3e588db800eb8b0aa2a7ad
Higashi Yonamine <e165714@ie.u-ryukyu.ac.jp>
2017-02-07 14:30:41 +0900

    definition of JPanel cheange long int short. but definition of Jbutton and processing　button are still long.

--- src/main/java/jp/ac/uryukyu/ie/e165714/ButtonMain.java
+++ src/main/java/jp/ac/uryukyu/ie/e165714/ButtonMain.java
@@ -4,21 +4,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class ButtonMain {
-    public void ButtonSyori(){
+    public void ButtonSyori() {
         //フレームのオブジェクト作成
         JFrame frame = new JFrame("文字入力アプリ");
         //パネルのオブジェクト作成
-        JPanel panel_A = new JPanel();
-        JPanel panel_K = new JPanel();
-        JPanel panel_S = new JPanel();
-        JPanel panel_T = new JPanel();
-        JPanel panel_H = new JPanel();
-        JPanel panel_M = new JPanel();
-        JPanel panel_Y = new JPanel();
-        JPanel panel_R = new JPanel();
-        JPanel panel_W = new JPanel();
-        JPanel panel_N = new JPanel();
-
+        JPanel panels[] = new JPanel[10];//JPanelのオブジェクトを生成するための領域を確保
+        for (int i = 0; i < 10; i++) {//panelsの要素にJPanel()オブジェクトを生成
+            panels[i] = new JPanel();
+        }
 
         //ボタンのオブジェクト作成
         JButton btn_a = new JButton("あ");
@@ -77,15 +70,9 @@ public class ButtonMain {
 
         JButton btn_wa = new JButton("わ");
         JButton btn_wi = new JButton("");
-        JButton btn_wu = new JButton("");
-        JButton btn_we = new JButton("");
         JButton btn_wo = new JButton("を");
-
+        JButton btn_w = new JButton("");
         JButton btn_nn = new JButton("ん");
-        JButton btn_n1 = new JButton("");
-        JButton btn_n2 = new JButton("");
-        JButton btn_n3 = new JButton("");
-        JButton btn_n4 = new JButton("");
 
         //テキストエリアのオブジェクトを生成#
         final JTextArea text = new JTextArea();
@@ -96,98 +83,90 @@ public class ButtonMain {
         //panel_ookiiパネルのオブジェクトを作成#
         JPanel panel_ookii = new JPanel();
 
-
         //ボタンまとめる用のJPanelを5行一列のグリッドレイアウトにする。
         botton_matomeru.setLayout(new GridLayout(1, 11));
 
         //パネルのレイアウトを5行1列のグリッドレイアウトにする
-        panel_A.setLayout(new GridLayout(5, 1));
-        panel_K.setLayout(new GridLayout(5, 1));
-        panel_S.setLayout(new GridLayout(5, 1));
-        panel_T.setLayout(new GridLayout(5, 1));
-        panel_N.setLayout(new GridLayout(5, 1));
-        panel_H.setLayout(new GridLayout(5, 1));
-        panel_M.setLayout(new GridLayout(5, 1));
-        panel_Y.setLayout(new GridLayout(5, 1));
-        panel_R.setLayout(new GridLayout(5, 1));
-        panel_W.setLayout(new GridLayout(5, 1));
+        for (int i = 0; i < 10; i++) {
+            panels[i].setLayout(new GridLayout(5, 1));
+        }
 
         //panel_ookiiのレイアウトをボーターレイアウトにする
         panel_ookii.setLayout(new BorderLayout());
 
         //パネルにボタンをセット
-        panel_A.add(btn_a);
-        panel_A.add(btn_i);
-        panel_A.add(btn_u);
-        panel_A.add(btn_e);
-        panel_A.add(btn_o);
+        panels[0].add(btn_a);
+        panels[0].add(btn_i);
+        panels[0].add(btn_u);
+        panels[0].add(btn_e);
+        panels[0].add(btn_o);
 
-        panel_K.add(btn_ka);
-        panel_K.add(btn_ki);
-        panel_K.add(btn_ku);
-        panel_K.add(btn_ke);
-        panel_K.add(btn_ko);
+        panels[1].add(btn_ka);
+        panels[1].add(btn_ki);
+        panels[1].add(btn_ku);
+        panels[1].add(btn_ke);
+        panels[1].add(btn_ko);
 
-        panel_S.add(btn_sa);
-        panel_S.add(btn_si);
-        panel_S.add(btn_su);
-        panel_S.add(btn_se);
-        panel_S.add(btn_so);
+        panels[2].add(btn_sa);
+        panels[2].add(btn_si);
+        panels[2].add(btn_su);
+        panels[2].add(btn_se);
+        panels[2].add(btn_so);
 
-        panel_T.add(btn_ta);
-        panel_T.add(btn_ti);
-        panel_T.add(btn_tu);
-        panel_T.add(btn_te);
-        panel_T.add(btn_to);
+        panels[3].add(btn_ta);
+        panels[3].add(btn_ti);
+        panels[3].add(btn_tu);
+        panels[3].add(btn_te);
+        panels[3].add(btn_to);
 
-        panel_N.add(btn_na);
-        panel_N.add(btn_ni);
-        panel_N.add(btn_nu);
-        panel_N.add(btn_ne);
-        panel_N.add(btn_no);
+        panels[4].add(btn_na);
+        panels[4].add(btn_ni);
+        panels[4].add(btn_nu);
+        panels[4].add(btn_ne);
+        panels[4].add(btn_no);
 
-        panel_H.add(btn_ha);
-        panel_H.add(btn_hi);
-        panel_H.add(btn_hu);
-        panel_H.add(btn_he);
-        panel_H.add(btn_ho);
+        panels[5].add(btn_ha);
+        panels[5].add(btn_hi);
+        panels[5].add(btn_hu);
+        panels[5].add(btn_he);
+        panels[5].add(btn_ho);
 
-        panel_M.add(btn_ma);
-        panel_M.add(btn_mi);
-        panel_M.add(btn_mu);
-        panel_M.add(btn_me);
-        panel_M.add(btn_mo);
+        panels[6].add(btn_ma);
+        panels[6].add(btn_mi);
+        panels[6].add(btn_mu);
+        panels[6].add(btn_me);
+        panels[6].add(btn_mo);
 
-        panel_Y.add(btn_ya);
-        panel_Y.add(btn_yi);
-        panel_Y.add(btn_yu);
-        panel_Y.add(btn_ye);
-        panel_Y.add(btn_yo);
+        panels[7].add(btn_ya);
+        panels[7].add(btn_yi);
+        panels[7].add(btn_yu);
+        panels[7].add(btn_ye);
+        panels[7].add(btn_yo);
 
-        panel_R.add(btn_ra);
-        panel_R.add(btn_ri);
-        panel_R.add(btn_ru);
-        panel_R.add(btn_re);
-        panel_R.add(btn_ro);
+        panels[8].add(btn_ra);
+        panels[8].add(btn_ri);
+        panels[8].add(btn_ru);
+        panels[8].add(btn_re);
+        panels[8].add(btn_ro);
 
-        panel_W.add(btn_wa);
-        panel_W.add(btn_wi);
-        panel_W.add(btn_wu);
-        panel_W.add(btn_we);
-        panel_W.add(btn_wo);
+        panels[9].add(btn_wa);
+        panels[9].add(btn_wi);
+        panels[9].add(btn_wo);
+        panels[9].add(btn_w);
+        panels[9].add(btn_nn);
 
         //パネルをボタンまとめるようパネルにセット
-        botton_matomeru.add(panel_N);
-        botton_matomeru.add(panel_W);
-        botton_matomeru.add(panel_R);
-        botton_matomeru.add(panel_Y);
-        botton_matomeru.add(panel_M);
-        botton_matomeru.add(panel_H);
-        botton_matomeru.add(panel_N);
-        botton_matomeru.add(panel_T);
-        botton_matomeru.add(panel_S);
-        botton_matomeru.add(panel_K);
-        botton_matomeru.add(panel_A);
+        botton_matomeru.add(panels[4]);
+        botton_matomeru.add(panels[9]);
+        botton_matomeru.add(panels[8]);
+        botton_matomeru.add(panels[7]);
+        botton_matomeru.add(panels[6]);
+        botton_matomeru.add(panels[5]);
+        botton_matomeru.add(panels[4]);
+        botton_matomeru.add(panels[3]);
+        botton_matomeru.add(panels[2]);
+        botton_matomeru.add(panels[1]);
+        botton_matomeru.add(panels[0]);
 
         //ボタンまとめるようパネルをpanel_ookiiのCENTERにセット
         panel_ookii.add(botton_matomeru);
@@ -437,4 +416,3 @@ public class ButtonMain {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
-
